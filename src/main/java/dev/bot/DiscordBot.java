1598a469351aef45e066c4465643d7a50bc7bb36
dominika <dominika_zurawska@gazeta.pl>
2020-11-08 12:09:42 +0100

    Initial commit

---
+++ src/main/java/dev/bot/DiscordBot.java
@@ -0,0 +1,9 @@
+package dev.bot;
+//Token: Nzc0NzgyNDQwNjI3MjQwOTYy.X6cyYA.a4djNU65vUNbDrU8CSRfRjubJQo
+//id 774736709686132746
+public class DiscordBot {
+    public static void main (String[] arguments){
+        System.out.println("1");
+
+    }
+}
